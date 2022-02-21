78ecb061d218227a7291ba4c6bfa722513712911
Louis Pilfold <louis@lpil.uk>
2022-02-21 17:38:18 +0000

    PostgreSQL on CI

--- bin/reset_dev_database.sh
+++ bin/reset_dev_database.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 set -eu
+export PGUSER=postgres
+export PGPASSWORD=postgres
 
 reset() {
   local database=$1
@@ -8,21 +10,7 @@ reset() {
   echo "Resetting $database"
   dropdb "$database" --if-exists
   createdb "$database" 2>&1 >/dev/null || true
-
-  for migration in $(ls migrations)
-  do
-    echo "> $migration"
-    pgexec "$database" "migrations/$migration/up.sql"
-  done
-}
-
-# https://petereisentraut.blogspot.com/2010/03/running-sql-scripts-with-psql.html
-pgexec() {
-  PGOPTIONS="--client-min-messages=warning" \
-    psql -X -1 -v ON_ERROR_STOP=1 \
-    --pset pager=off \
-    -d "$1" \
-    --file "$2"
+  ./bin/migrate.sh $database
 }
 
 reset gleam_todomvc_dev
